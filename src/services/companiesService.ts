c9e7e63e059ee52c93dc515d4a30ea90f3a27161
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-03-30 22:46:56 +0200

    [WIP] en cours sur affichage des infos utilisateur

--- src/services/companiesService.ts
+++ src/services/companiesService.ts
@@ -5,7 +5,7 @@ import {companiesRepository} from "./layoutService";
 
 export default {
   createCompany: async (company: CompaniesInput): Promise<Companies> => {
-    const user = await userService.getByEmail('seveste.brandon@gmail.com')
+    const user = await userService.getByEmail('joel.miller@gmail.com')
     const newCompany = new Companies();
     newCompany.name = company.name;
     newCompany.siret = company.siret;
