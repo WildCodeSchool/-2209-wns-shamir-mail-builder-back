5a23c7b63995e94ad5cf5d703774235d27d713b7
DieumegardGregory <dieumegard.gregory@gmail.com>
2023-04-01 13:55:41 +0200

    [FIX] tests

--- src/tools/utils.ts
+++ src/tools/utils.ts
@@ -10,11 +10,11 @@ dotenv.config();
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "db",
+  host: process.env.DB_HOST,
   port: 5432,
-  username: "bragagre",
-  password: "example",
-  database: "mail-builder",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   entities: [
     User,
